9daa1ccf59fb2de7fdb83d3386ee265ea5806aa6
HaiAnh <haianhnguyen12082001>
2021-10-18 16:50:46 +0700

    index.html

---
+++ .history/assets/js/main_20211016233850.js
@@ -0,0 +1,51 @@
+AOS.init();
+$(document).ready(function () {
+    // BANNER SLIDER
+    $(".banner__slides").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 3000,
+    });
+    
+    //CLICK NEXT PROJEC
+    
+    $(".villa__list").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: false,
+        // dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        arrows:true,
+            // prevArrow: '<button class="click__left"><i class="fas fa-arrow-left btn"></i></button>',
+            // nextArrow: '<button class="click__right"><i class="fas fa-arrow-right btn"></i></button>',
+        prevArrow: $('  .click__left'),
+        nextArrow: $(' .click__right'),
+
+    });
+    $(".project1__list").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: false,
+        // dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        // arrows:false,
+            // prevArrow: '<button class="click__left"><i class="fas fa-arrow-left btn"></i></button>',
+            // nextArrow: '<button class="click__right"><i class="fas fa-arrow-right btn"></i></button>',
+        prevArrow: $('.click__left'),
+        nextArrow: $('.click__right'),
+
+    });
+
+});
---
+++ .history/assets/js/main_20211018155434.js
@@ -0,0 +1,143 @@
+AOS.init();
+$(document).ready(function () {
+    // BANNER SLIDER
+    $(".banner__slides").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 3000,
+    });
+    
+    //CLICK NEXT PROJEC
+    
+    $(".villa__list").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: false,
+        // dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        arrows:true,
+            prevArrow: '<button class="click__left"><i class="fas fa-arrow-left btn"></i></button>',
+            nextArrow: '<button class="click__right"><i class="fas fa-arrow-right btn"></i></button>',
+        // prevArrow: $('  .click__left'),
+        // nextArrow: $(' .click__right'),
+
+    });
+    $(".project1__list").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: false,
+        // dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        // arrows:false,
+            prevArrow: '<button class="click__left"><i class="fas fa-arrow-left btn"></i></button>',
+            nextArrow: '<button class="click__right"><i class="fas fa-arrow-right btn"></i></button>',
+        // prevArrow: $('.click__left'),
+        // nextArrow: $('.click__right'),
+
+    });
+    $(".feedback__list").slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        speed: 2000,
+        autoplaySpeed: 3000,
+        prevArrow: '<button class="prevFeedback"><i class="fas fa-arrow-left btn"></i></button>',
+        nextArrow: '<button class="nextFeedback"><i class="fas fa-arrow-right btn"></i></button>',
+        responsive:[
+            {
+                breakpoint: 768,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    speed: 2000,
+                    autoplaySpeed: 3000,
+                  infinite: true,
+                  dots: true
+                }
+              },
+        ]
+    });
+    
+    $(".product").slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        speed: 2000,
+        autoplaySpeed: 3000,
+        dots:true,
+        responsive:[
+            {
+                breakpoint: 1024,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    arrows: false,
+                    autoplay: true,
+                    speed: 2000,
+                    autoplaySpeed: 3000,
+                //   infinite: true,
+                //   dots: true
+                }
+              },
+            {
+                breakpoint: 767,
+                settings: {
+                    arrows: false,
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                }
+              },
+        ]
+    });
+    $(".news__list").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,  
+        speed: 2000,
+        autoplaySpeed: 3000,
+        responsive:[
+            {
+                breakpoint: 1024,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    speed: 2000,
+                    autoplaySpeed: 3000,
+                //   infinite: true,
+                //   dots: true
+                }
+              },
+            {
+                breakpoint: 767,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                }
+              },
+        ]
+    });
+
+    ///show menu///
+    $('.bars__mobi').click(function(){
+        $('.header__list').toggleClass('active');
+    })
+
+});
